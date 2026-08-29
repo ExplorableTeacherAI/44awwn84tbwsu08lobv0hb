ace589f6c51aeff72a95d57862bedb08259e0419
MathVibe <mathvibe@Jiannans-MacBook-Air-2.local>
2026-08-29 20:26:34 +0800

    @block:coordinate-intro-promise
    keep this pointwi...

--- src/data/sections/coordinateGeometryIntro.tsx
+++ src/data/sections/coordinateGeometryIntro.tsx
@@ -26,10 +26,47 @@ export const coordinateGeometryIntroBlocks: ReactElement[] = [
         <Block id="coordinate-intro-promise" padding="sm">
             <EditableParagraph id="para-coordinate-intro-promise" blockId="coordinate-intro-promise">
                 That is coordinate geometry: turning positions on a grid into numbers you can
-                calculate with. Here you will work out the distance between two points, find the
-                halfway point between them, describe a straight line with an equation, and do the
-                same for a circle. If you can read a point off an x-y grid, you have everything
-                you need to start.
+                calculate with. Here is what you will be able to do by the end.
+            </EditableParagraph>
+        </Block>
+    </StackLayout>,
+
+    <StackLayout key="layout-coordinate-intro-point-distance" maxWidth="xl">
+        <Block id="coordinate-intro-point-distance" padding="sm">
+            <EditableParagraph id="para-coordinate-intro-point-distance" blockId="coordinate-intro-point-distance">
+                • Work out the distance between two points.
+            </EditableParagraph>
+        </Block>
+    </StackLayout>,
+
+    <StackLayout key="layout-coordinate-intro-point-midpoint" maxWidth="xl">
+        <Block id="coordinate-intro-point-midpoint" padding="sm">
+            <EditableParagraph id="para-coordinate-intro-point-midpoint" blockId="coordinate-intro-point-midpoint">
+                • Find the halfway point between them.
+            </EditableParagraph>
+        </Block>
+    </StackLayout>,
+
+    <StackLayout key="layout-coordinate-intro-point-line" maxWidth="xl">
+        <Block id="coordinate-intro-point-line" padding="sm">
+            <EditableParagraph id="para-coordinate-intro-point-line" blockId="coordinate-intro-point-line">
+                • Describe a straight line with an equation.
+            </EditableParagraph>
+        </Block>
+    </StackLayout>,
+
+    <StackLayout key="layout-coordinate-intro-point-circle" maxWidth="xl">
+        <Block id="coordinate-intro-point-circle" padding="sm">
+            <EditableParagraph id="para-coordinate-intro-point-circle" blockId="coordinate-intro-point-circle">
+                • Do the same for a circle.
+            </EditableParagraph>
+        </Block>
+    </StackLayout>,
+
+    <StackLayout key="layout-coordinate-intro-prerequisite" maxWidth="xl">
+        <Block id="coordinate-intro-prerequisite" padding="sm">
+            <EditableParagraph id="para-coordinate-intro-prerequisite" blockId="coordinate-intro-prerequisite">
+                If you can read a point off an x-y grid, you have everything you need to start.
             </EditableParagraph>
         </Block>
     </StackLayout>,
